87cfaa56b1365c2c71bc48015d3ca167e3293e8e
MatasMatulionis <99017850+MatasMatulionis@users.noreply.github.com>
2024-04-24 09:52:02 +0300

    feat: page and conflict solved

--- prometheus-app/app/[locale]/page.tsx
+++ prometheus-app/app/[locale]/page.tsx
@@ -1,11 +1,19 @@
 import { useTranslations } from "next-intl";
 import "styles/index.scss";
 
+
 export default function Index() {
   const t = useTranslations("Index");
   return (
-    <div>
-      <h1>{t("title")}</h1>
-    </div>
+    <>
+      <section>
+        <h1 className="slogan">{t("title")}</h1>
+        <div className="contact-button">
+          <span>Parašykite mums</span>
+          <img src="/Icons/Arrow_right.png"></img>
+        </div>
+        
+      </section>
+    </>
   );
 }
