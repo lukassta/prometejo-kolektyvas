ca48e738e58613f20a4249f112d7e5164b98ac22
MatasMatulionis <99017850+MatasMatulionis@users.noreply.github.com>
2024-06-14 13:39:42 +0300

    feat: Added gitignore, services almost done, prettier fixed

--- prometheus-app/app/[locale]/page.tsx
+++ prometheus-app/app/[locale]/page.tsx
@@ -1,18 +1,30 @@
 import { useTranslations } from "next-intl";
 import "styles/index.scss";
 
-
 export default function Index() {
   const t = useTranslations("Index");
   return (
     <>
-      <section>
+      <section className="extra-margin">
         <h1 className="slogan">{t("title")}</h1>
         <div className="contact-button">
           <span>Parašykite mums</span>
           <img src="/Icons/Arrow_right.png"></img>
         </div>
-        
+      </section>
+      <section className="box">
+        <div className="fifty">
+          <h2>Paslaugos</h2>
+          <ul className="square">
+            <li>Tinklapių kūrimas</li>
+            <li>Tinklapių atnaujinimas</li>
+            <li>Tinklapių koregavimas</li>
+            <li>Tinklapių išlaikymas</li>
+          </ul>
+        </div>
+        <div className="fifty">
+          <button className="orange-button">Sužinokite daugiau</button>
+        </div>
       </section>
     </>
   );
